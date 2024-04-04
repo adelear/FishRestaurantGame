2f10ff354a135846343ce40baa5355c6f7ed0f42
Rae Leda <veronica.rleda@gmail.com>
2024-04-04 12:03:07 -0400

    Shorter game time

--- Assets/Scripts/Managers/FishSpawner.cs
+++ Assets/Scripts/Managers/FishSpawner.cs
@@ -15,7 +15,7 @@ public class FishSpawner : MonoBehaviour
     private float spawnTimer = 0f;
     private float totalTimeElapsed = 0f;
     private float currentSpawnInterval;
-    private float gameTime = 600f; 
+    private float gameTime = 300; 
 
     private float timeSinceLastAdjustment = 0f;
     private const float timeBetweenAdjustments = 120f; 
@@ -66,7 +66,7 @@ public class FishSpawner : MonoBehaviour
                     spawnTimer = 0f;
                 }
 
-                if (totalTimeElapsed >= 240f && currentSpawnInterval != additionalSpawnIntervalMax)
+                if (totalTimeElapsed >= 120 && currentSpawnInterval != additionalSpawnIntervalMax)
                 {
                     AdjustSpawnInterval();
                 }
